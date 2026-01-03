a5369eb1c8f90737eae852c978175ef46f19dc99
prasanna nagesh <prasannachat@gmail.com>
2026-01-03 15:42:49 +0530

    code restructuring and changes in transactions model to get raw data from public dataset

---
+++ models/raw/transactions.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='incremental',
+incremental_strategy='insert_overwrite',
+partition_by={'field': 'block_timestamp',
+  'data_type': 'timestamp'
+}
+) }}
+
+SELECT
+  *
+FROM
+  {{source ('ethereum_raw','transactions')}}
+WHERE
+  DATE(block_timestamp) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
+  AND CURRENT_DATE()
+
+  {% if is_incremental() %}
+  WHERE block_timestamp >= (
+    SELECT MAX(block_timestamp) FROM {{ this }}
+  )
+{% endif %}
